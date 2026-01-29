ce8d24b0863b9167e759c1ae066ebe65a8a141ec
the-elig <elidg@uci.edu>
2026-01-28 16:18:38 -0800

    did basic "received_command" logic

--- new-game-project/scripts/command_line.gd
+++ new-game-project/scripts/command_line.gd
@@ -1,11 +1,30 @@
 extends Node
+class_name CommandLine
 
+@export var line_edit: LineEdit 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# returns true if "valid" command
+func received_command(input: String):
+	if input.contains("/") and input.contains("@"):
+		# general formatting
+		input = input.remove_chars(" ") #removes spaces
+		input = input.to_lower() #to lowercase
+		
+		
+		# separating arguments
+		var slash = input.find("/") #index of slash
+		var at = input.find("@") #index of at symbol
+		
+		var object = input.substr(0, slash) 
+		var action = input.substr(
+			slash + 1, 
+			at - object.length() - 1)
+		var location = input.substr(at + 1)
+		
+		print(object)
+		print(action)
+		print(location)
+	else:
+		print("invalid command")
+	
+	line_edit.text = "" #clears command after execution
