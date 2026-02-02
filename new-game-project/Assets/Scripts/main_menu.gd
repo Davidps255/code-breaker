4a82032462b0dc17e7fec2bb16a50c4235f43eb5
Justin Li <jdli3@uci.edu>
2026-02-01 21:12:33 -0800

    added peak music
    - new font bc default is kinda ugly
    - things are more centered
    - sliders for music and sfx added
    - options menu now scrolls

--- new-game-project/Assets/Scripts/main_menu.gd
+++ new-game-project/Assets/Scripts/main_menu.gd
@@ -1,21 +1,61 @@
 extends Control
 
 @export var start_scene: PackedScene
-const MASTER_INDEX = 0
+
+# Volume sliders
+@onready var master = $OptionsPanel/ScrollContainer/VBoxContainer/MasterPanel/MasterSlider
+@onready var music = $OptionsPanel/ScrollContainer/VBoxContainer/MusicPanel/MusicSlider
+@onready var sfx = $OptionsPanel/ScrollContainer/VBoxContainer/SFXPanel/SFXSlider
 
 func _ready() -> void:
-	$OptionsPanel.hide()
-	$AudioStreamPlayer.play()
+	$Music.play()
+	master.value = SoundManager.get_volume("Master")
+	music.value = SoundManager.get_volume("Music")
+	sfx.value = SoundManager.get_volume("SFX")
+	var buttons: Array = get_tree().get_nodes_in_group("Button")
+	for button in buttons:
+		button.pressed.connect(_on_button_pressed)
+
+
+func _on_button_pressed(_hi=0) -> void:
+	$ButtonSFX.play()
 
 
 func _on_start_button_pressed() -> void:
+	TransitionScreen.transition()
+	fade_volume(1)
+	await TransitionScreen.transition_finished
 	get_tree().change_scene_to_packed(start_scene)
 
 
+func fade_volume(time: float) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($Music, "volume_db", -80.0, time)
+
+
 func _on_options_button_pressed() -> void:
-	$OptionsPanel.visible = not $OptionsPanel.visible
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
+	if $OptionsPanel.position.x < 1150.0:
+		tween.tween_property($OptionsPanel,"position", Vector2(1160, $OptionsPanel.position.y), 0.5)
+	else:
+		tween.tween_property($OptionsPanel, "position", Vector2(663, $OptionsPanel.position.y), 0.5)
 
 
 func _on_master_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(MASTER_INDEX, linear_to_db(value))
-	# print(AudioServer.get_bus_volume_linear(MASTER_INDEX))
+	SoundManager.set_volume("Master", value)
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	SoundManager.set_volume("Music", value)
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	SoundManager.set_volume("SFX", value)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_music_finished() -> void:
+	$Music.play()
