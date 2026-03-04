e5e6df89f47958dd34c981d1e2e5caa027f1450b
Davidps255 <davidps@uci.edu>
2026-03-04 15:18:58 -0800

    Goggle Frame

--- new-game-project/Assets/Scripts/goggles_overlay.gd
+++ new-game-project/Assets/Scripts/goggles_overlay.gd
@@ -10,3 +10,18 @@ func _physics_process(_delta) -> void:
 		else:
 			goggles_on = true
 			$ColorRect.visible = true
+	
+	if goggles_on:
+		var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+		var cam: Camera3D = get_viewport().get_camera_3d()
+		
+		$RayCast3D.target_position = cam.project_ray_normal(mouse_pos) * 1000
+		
+		if $RayCast3D.is_colliding():
+			var result: Object = $RayCast3D.get_collider()
+			var script: Script = result.get_script()
+			
+			if script:
+				print("Script")
+			else:
+				print("No script")
